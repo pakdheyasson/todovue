0aae99e6c01b51ff7b0d799b529e5fe6732f3860
pakdheyasson <110944882+pakdheyasson@users.noreply.github.com>
2022-09-28 10:54:08 +0700

    Initial commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                    <h1>{{message}}</h1>
+                    <span v-if="type=='A'">If type is A</span>
+                    <span v-else-if="type=='B'">If type is B</span>
+                    <span v-else>If type is other then A or B</span>
+                    <br>
+                    <ol>
+                        <li v-for="(todo, index) in todos" :key="index">
+                            {{todo.text}}
+                        </li>
+                    </ol>
+                    <br>
+                    <h4>input text</h4>
+                    <input v-model="input" placeholder="Input Text">
+                    <h4>Show User Input:</h4>
+                    <p>{{input}}</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data:function(){
+            return{
+                message:"Haiiii....ini data baru",
+                type:"C",
+                todos:[
+                    {text:'learn javascript'},
+                    {text:'learn vue'},
+                    {text:'built something awasome'}
+                ],
+                input:null
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/ToDoComponent.vue
@@ -0,0 +1,47 @@
+
+import { message } from 'laravel-mix/src/Log';
+<template>
+    <div class="container">
+        <h1>Nggawe To Do</h1>
+        <form class="form">
+            <input type="text" v-model="input">
+            <button type="button" @click="addList">Submit</button>
+        </form>
+        <ul class="list">
+            <li v-for="(item, index) in list" :key="index">
+                {{item.title}}<button class="delete" @click="deleteList(index)">Hapus</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data:function(){
+        return {
+         list: [
+            {title:"menyapu"},
+            {title:"cuci piring"},
+            {title:"ngepel"},
+         ],
+         input:""
+        }
+    },
+    methods: {
+                addList () {
+                    let newList= {
+                        title : this.input 
+                    }
+                    this.list.push (newList)
+                    this.input = ""
+                },
+                deleteList(index){
+                    this.list.splice(index,1)
+                }
+            },
+            mounted() {
+    console.log('Component mounted.')
+    },
+}
+   
+</script>
+
